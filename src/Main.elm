f77161e3c846cf599cf3a3925cf53cc171188c97
Ken Youens-Clark <kyclark@gmail.com>
2018-03-07 15:35:09 -0700

    added a bunch more fields

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Dialog
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Encode as JE
 
 
 ---- MODEL ----
@@ -14,10 +15,30 @@ type alias Model =
     , version : String
     , available : Bool
     , checkpointable : Bool
+    , defaultMemoryPerNode : Int
+    , defaultProcessorsPerNode : Int
+    , defaultMaxRunTime : String
+    , defaultNodeCount : Int
+    , defaultQueue : String
+    , deploymentPath : String
+    , deploymentSystem : String
+    , executionSystem : String
+    , executionType : String
+    , helpURI : String
+    , label : String
+    , longDescription : String
+    , modules : List String
+    , ontology : List String
+    , parallelism : String
+    , shortDescription : String
+    , tags : List String
+    , templatePath : String
+    , testPath : String
     , inputs : List AppInput
     , parameters : List AppParam
     , showJson : Bool
     , showInputForm : Bool
+    , error : Maybe String
     }
 
 
@@ -32,14 +53,34 @@ type alias AppParam =
 
 
 initialModel =
-    { appName = "MyNewApp"
+    { appName = "my_new_app"
     , version = "0.0.1"
     , available = True
     , checkpointable = False
+    , defaultMemoryPerNode = 32
+    , defaultProcessorsPerNode = 16
+    , defaultMaxRunTime = "12:00:00"
+    , defaultNodeCount = 1
+    , defaultQueue = "normal"
+    , deploymentPath = "user/applications/app_name-version/stampede"
+    , deploymentSystem = "data.iplantcollaborative.org"
+    , executionSystem = "tacc-stampede2-user"
+    , executionType = "HPC"
+    , helpURI = "http://google.com"
+    , label = "MyNewApp"
+    , longDescription = ""
+    , modules = [ "tacc-singularity", "launcher" ]
+    , ontology = [ "imicrobe" ]
+    , parallelism = "SERIAL"
+    , shortDescription = ""
+    , tags = [ "imicrobe" ]
+    , templatePath = "template.sh"
+    , testPath = "test.sh"
     , inputs = []
     , parameters = []
     , showJson = False
     , showInputForm = False
+    , error = Nothing
     }
 
 
@@ -95,6 +136,18 @@ updateApp model fldName newValue =
         "version" ->
             { model | version = newValue }
 
+        "defaultMemoryPerNode" ->
+            let
+                ( newMem, error ) =
+                    case String.toInt newValue of
+                        Ok n ->
+                            ( n, Nothing )
+
+                        _ ->
+                            ( model.defaultMemoryPerNode, Just "Not a number" )
+            in
+            { model | defaultMemoryPerNode = newMem }
+
         _ ->
             model
 
@@ -103,35 +156,42 @@ updateApp model fldName newValue =
 ---- VIEW ----
 
 
-generateJson model =
-    "{"
-        ++ "\"name\": \""
-        ++ model.appName
-        ++ "\","
-        ++ "\"version\": \""
-        ++ model.version
-        ++ "\","
-        ++ "\"available\": "
-        ++ (if model.available then
-                "true"
-            else
-                "false"
-           )
-        ++ ","
-        ++ "\"checkpointable\": "
-        ++ (if model.checkpointable then
-                "true"
-            else
-                "false"
-           )
-        ++ "}"
+encodeApp : Model -> String
+encodeApp model =
+    JE.encode 4
+        (JE.object
+            [ ( "name", JE.string model.appName )
+            , ( "version", JE.string model.version )
+            , ( "shortDescription", JE.string model.shortDescription )
+            , ( "longDescription", JE.string model.longDescription )
+            , ( "available", JE.bool model.available )
+            , ( "checkpointable", JE.bool model.checkpointable )
+            , ( "defaultMemoryPerNode", JE.int model.defaultMemoryPerNode )
+            , ( "defaultProcessorsPerNode", JE.int model.defaultProcessorsPerNode )
+            , ( "defaultMaxRunTime", JE.string model.defaultMaxRunTime )
+            , ( "defaultNodeCount", JE.int model.defaultNodeCount )
+            , ( "defaultQueue", JE.string model.defaultQueue )
+            , ( "deploymentPath", JE.string model.deploymentPath )
+            , ( "deploymentSystem", JE.string model.deploymentSystem )
+            , ( "executionSystem", JE.string model.executionSystem )
+            , ( "executionType", JE.string model.executionType )
+            , ( "helpURI", JE.string model.helpURI )
+            , ( "label", JE.string model.label )
+            , ( "parallelism", JE.string model.parallelism )
+            , ( "templatePath", JE.string model.templatePath )
+            , ( "testPath", JE.string model.testPath )
+            , ( "modules", JE.list (List.map JE.string model.modules) )
+            , ( "tags", JE.list (List.map JE.string model.tags) )
+            , ( "ontology", JE.list (List.map JE.string model.ontology) )
+            ]
+        )
 
 
 viewJson model =
     let
         json =
             if model.showJson then
-                generateJson model
+                encodeApp model
             else
                 ""
 
@@ -147,8 +207,19 @@ viewJson model =
                     Just
                         { closeMessage = Nothing
                         , containerClass = Nothing
-                        , header = Just (text model.appName)
-                        , body = Just (p [] [ text json ])
+                        , header =
+                            Just
+                                (text
+                                    (model.appName
+                                        ++ "-"
+                                        ++ model.version
+                                    )
+                                )
+                        , body =
+                            Just
+                                (div [ style [ ( "overflow-y", "auto" ) ] ]
+                                    [ pre [] [ text json ] ]
+                                )
                         , footer =
                             Just
                                 (button
@@ -233,47 +304,266 @@ viewInputs model =
 
 view : Model -> Html Msg
 view model =
+    let
+        errorMsg =
+            case model.error of
+                Just err ->
+                    "Error: " ++ err
+
+                _ ->
+                    ""
+    in
     div []
         [ h1 [] [ text "Appetizer" ]
+        , div [] [ text errorMsg ]
         , Html.form []
             [ div [ class "form-group", onInput (UpdateApp "appName") ]
                 [ Html.label [] [ text "App Name" ]
                 , Html.input
                     [ type_ "text"
                     , name "appName"
-                    , placeholder model.appName
+                    , defaultValue model.appName
                     , class "form-control"
                     ]
                     []
                 ]
-            ]
-        , div [ class "form-group", onInput (UpdateApp "version") ]
-            [ Html.label [] [ text "Version" ]
-            , Html.input
-                [ type_ "text"
-                , name "version"
-                , placeholder model.version
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "label")
                 ]
-                []
-            ]
-        , div [ class "form-group" ]
-            [ Html.label [] [ text "Available" ]
-            , checkbox ToggleAvailable model.available
-            ]
-        , div [ class "form-group" ]
-            [ Html.label [] [ text "Checkpointable" ]
-            , checkbox ToggleCheckpointable model.checkpointable
-            ]
-        , div [ class "form-group" ]
-            [ Html.label []
-                [ text
-                    ("Inputs ("
-                        ++ toString (List.length model.inputs)
-                        ++ ")"
-                    )
+                [ Html.label [] [ text "Label" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "label"
+                    , defaultValue model.label
+                    , class "form-control"
+                    ]
+                    []
                 ]
+            , div [ class "form-group", onInput (UpdateApp "version") ]
+                [ Html.label [] [ text "Version" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "version"
+                    , defaultValue model.version
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "shortDescription")
+                ]
+                [ Html.label [] [ text "Short Description" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "label"
+                    , defaultValue model.shortDescription
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "longDescription")
+                ]
+                [ Html.label [] [ text "Long Description" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "label"
+                    , defaultValue model.longDescription
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div [ class "form-group" ]
+                [ Html.label [] [ text "Available" ]
+                , checkbox ToggleAvailable model.available
+                ]
+            , div [ class "form-group" ]
+                [ Html.label [] [ text "Checkpointable" ]
+                , checkbox ToggleCheckpointable model.checkpointable
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "defaultMemoryPerNode")
+                ]
+                [ Html.label [] [ text "Default Memory Per Node" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultMemoryPerNode"
+                    , defaultValue (toString model.defaultMemoryPerNode)
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "defaultProcessorsPerNode")
+                ]
+                [ Html.label [] [ text "Default Processors Per Node" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultProcessorsPerNode"
+                    , defaultValue (toString model.defaultProcessorsPerNode)
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "defaultMaxRunTime")
+                ]
+                [ Html.label [] [ text "Default Max Run Time" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultMaxRunTime"
+                    , defaultValue model.defaultMaxRunTime
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "defaultNodeCount")
+                ]
+                [ Html.label [] [ text "Default Node Count" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultNodeCount"
+                    , defaultValue (toString model.defaultNodeCount)
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "defaultQueue")
+                ]
+                [ Html.label [] [ text "Default Queue" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultQueue"
+                    , defaultValue model.defaultQueue
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "deploymentPath")
+                ]
+                [ Html.label [] [ text "Deployment Path" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "defaultQueue"
+                    , defaultValue model.deploymentPath
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "deploymentSystem")
+                ]
+                [ Html.label [] [ text "Deployment System" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "deploymentSystem"
+                    , defaultValue model.deploymentSystem
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "executionSystem")
+                ]
+                [ Html.label [] [ text "Execution System" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "executionSystem"
+                    , defaultValue model.executionSystem
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "executionType")
+                ]
+                [ Html.label [] [ text "Execution Type" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "executionType"
+                    , defaultValue model.executionType
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "helpURI")
+                ]
+                [ Html.label [] [ text "Help URI" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "helpURI"
+                    , defaultValue model.helpURI
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "parallelism")
+                ]
+                [ Html.label [] [ text "Parallelism" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "parallelism"
+                    , defaultValue model.parallelism
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "templatePath")
+                ]
+                [ Html.label [] [ text "Template Path" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "templatePath"
+                    , defaultValue model.templatePath
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div
+                [ class "form-group"
+                , onInput (UpdateApp "testPath")
+                ]
+                [ Html.label [] [ text "Test Path" ]
+                , Html.input
+                    [ type_ "text"
+                    , name "testPath"
+                    , defaultValue model.testPath
+                    , class "form-control"
+                    ]
+                    []
+                ]
+            , div [ class "form-group" ]
+                [ Html.label []
+                    [ text
+                        ("Inputs ("
+                            ++ toString (List.length model.inputs)
+                            ++ ")"
+                        )
+                    ]
+                ]
+            , viewInputs model
             ]
-        , viewInputs model
         , viewJson model
         ]
 
@@ -294,6 +584,7 @@ checkbox msg state =
         [ type_ "checkbox"
         , onClick msg
         , checked state
+        , class "form-control"
         ]
         []
 
